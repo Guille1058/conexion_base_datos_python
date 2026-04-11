a0f47e06df060d8dc9f5cf0c87f0d4a6e6fd72e9
Guillermo <guillermocuaresmautrerapj@gmail.com>
2026-04-11 17:28:27 +0200

    V1 - Programa Funcional

--- main.py
+++ main.py
@@ -1,37 +1,98 @@
 import pymysql
+import keyboard
+import os
+import ctypes
+import platform
+
+def es_administrador():
+    try:
+        # Para sistemas basados en Unix (Linux, macOS)
+        if platform.system() != "Windows":
+            return os.getuid() == 0
+        
+        # Para Windows
+        else:
+            return ctypes.windll.shell32.IsUserAnAdmin() != 0
+    except AttributeError:
+        return False
+
+# Función para cerrar el programa inmediatamente
+
+def salir_emergencia():
+    print("\nSaliendo del programa de forma segura...")
+    # Cerramos la conexión si existe antes de forzar el cierre
+    if 'conexion' in globals() and conexion:
+        conexion.close()
+    os._exit(0) # Fuerza la salida inmediata del proceso
+
+# Funciones input
+
+def creacion_bbdd(cursor):
+    # Crear y usar la base de datos
+    cursor.execute("CREATE DATABASE IF NOT EXISTS base_de_datos")
+    cursor.execute("USE base_de_datos")
+
+    # Crear tabla
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Gente(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nombre VARCHAR(50) NOT NULL,
+            apellidos VARCHAR(50) NOT NULL,
+            contraseña VARCHAR(10) NOT NULL,
+            perfil VARCHAR(30) NOT NULL
+        )
+    """)
+
+def introducir_datos():
+    v1 = input("Introduce un nombre para el usuario -> ")
+    while len(v1)< 1 or len(v1) > 50:
+        v1 = input("¡¡ERROR!! (VALOR NO VALIDO) -- Introduce un nombre para el usuario\nLongitud de caracteres permitida 1-50 -> ")
+    v2 = input("Introduce los apellidos para el usuario -> ")
+    while len(v2) < 1 or len(v2) > 50:
+        v2 = input("¡¡ERROR!! (VALOR NO VALIDO) -- Introduce apellidos para el usuario\nLongitud de caracteres permitida 1-50 -> ")
+    v3 = input("Introduce la contraseña para el usuario -> ")
+    while len(v3) < 1 or len(v3) > 10:
+        v3 = input("¡¡ERROR!! (VALOR NO VALIDO) -- Introduce una contraseña para el usuario\nLongitud de caracteres permitida 1-10 -> ")
+    v4 = input("Introduce el perfil para el usuario -> ")
+    while len(v4) < 1 or len(v4) > 30:
+        v4 = input("¡¡ERROR!! (VALOR NO VALIDO) -- Introduce un perfil para el usuario (ej: admin / editor / etc.)\nLongitud de caracteres permitida 1-30 -> ")
+    return [v1, v2, v3, v4]
+
+if not es_administrador():
+    print("¡ERROR! - Debes ejecutar este programa como administrador o sudo, vuelve a intentarlo más tarde.")
+    os._exit(0)
 
 conexion = None
+# Registramos el 'hotkey'. En cualquier momento que presiones la tecla -ESC-, se cerrará.
+keyboard.add_hotkey('esc', salir_emergencia)
 
 try:
     conexion = pymysql.connect(
         host="localhost",
         user="root",
         password="",
-        database="base_de_datos",
         port=3306
     )
     print("Conexión correcta")
-
     cursor = conexion.cursor()
-
-    # Crear tabla
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS usuarios(
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            nombre VARCHAR(50),
-            email VARCHAR(50)
-        )
-    """)
+    creacion_bbdd(cursor) #Función para crear la base de datos.
 
     # Insertar datos (columnas correctas)
-    
-    sql = "INSERT INTO usuarios (nombre, email, telefono, dni) VALUES (%s, %s, %s, %s)"
-    valores = ("Guillermo", "gcu25gr016@student.esgerencia.com", "658281840", "78112397G")
+    sql = "INSERT INTO Gente (nombre, apellidos, contraseña, perfil) VALUES (%s, %s, %s, %s)"
 
-    cursor.execute(sql, valores)
-    conexion.commit()
+    # Request al usuario
+    i = int(input("INTRODUCE LA CANTIDAD DE USUARIOS A ALMACENAR EN LA BASE DE DATOS. -> "))
+    contador = 0
 
-    print("Datos insertados correctamente")
+    while contador < i:
+        vector = introducir_datos()
+        valores = (vector[0], vector[1], vector[2], vector[3])
+        cursor.execute(sql, valores)
+        conexion.commit()
+        print("------------------------")
+        print("*** Datos insertados correctamente ***")
+        print("------------------------")
+        contador+=1
 
 except Exception as err:
     print("Error:", err)
